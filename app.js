a5d14032f461b6d2de576b206d0c8ce880acbbfa
Christopher Cole <christophercole1987@icloud.com>
2019-01-11 10:05:04 -0500

    Added more code; troubleshooting edit and delete buttons and alert message not working

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const app = express();
-app.use(bodyParser.json());
 const path = require('path');
+const Joi = require('joi');
 
 const db = require("./db");
 const collection  = "todo";
+const app = express();
+
+const schema = Joi.object().keys({
+    todo : Joi.string().required()
+});
+
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
@@ -37,26 +43,44 @@ app.put('/:id',(req,res)=>{
 
 app.post('/',(req,res)=>{
     const userInput = req.body;
-    db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
-        if(err)
-            console.log(err);
-        else {
-            res.json({result : result, document : result.ops[0]});
+
+    Joi.validate(userInput,schema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
         }
-    });
+        else {
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err)
+                    console.log(err);
+                else {
+                    res.json({result : result, document : result.ops[0]});
+                }
+            });
+        }
+    })
+
 });
 
 app.delete('/:id',(req,res)=>{
     const todoId = req.params.id;
 
-    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoId)},(err,result)=>{
-        if(err)
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoId)},(err,result)=> {
+        if (err)
             console.log(err)
-        else
-        {
+        else {
             res.json(result);
         }
-    })
+    });
+});
+
+app.use((err,req,res,next)=>{
+    res.status(err.status).json({
+        error : {
+            message : err.message
+        }
+    });
 })
 
 db.connect((err)=>{
@@ -65,8 +89,8 @@ db.connect((err)=>{
         process.exit(1);
     }
     else {
-        app.listen(3000,()=>{
-            console.log('Connected to database, app listening on port 3000')
+        app.listen(2000,()=>{
+            console.log('Connected to database, app listening on port 2000')
         });
     }
 });
